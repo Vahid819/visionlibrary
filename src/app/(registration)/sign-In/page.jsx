a6125251e54409a3d261b713f4ab1d665d40f8fa
vahid819 <vahidmomin072@gmail.com>
2025-01-10 11:01:14 +0530

    add animation on buttons

--- src/app/(registration)/sign-In/page.jsx
+++ src/app/(registration)/sign-In/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import SigninForm from '@/components/SigninForm'
+import Link from 'next/link'
 
 function Signin() {
   return (
-    <div>
-        
+    <div className='flex justify-center items-center md:h-[100vh]'>
+      <div className='md:w-[30%] m-auto md:h-[85%] px-12 py-5 bg-white rounded-lg'>
+        <h1 className='text-center text-3xl'>Sign In</h1>
+        <SigninForm />
+        <h3 className='text-gray-500 text-sm py-2 flex items-center h-10 gap-1'>If your account are not created then go to <Link href={"/sign-Up"} className='hover:text-blue-700 hover:text-lg transition-all duration-300 ease-in-out'>Sign Up</Link></h3>
+      </div>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ function Navbar() {
             </div>
             <div className='flex justify-around w-[15%]'>
                 <div className='signin' id='signin'>
-                <Link href={"sign-In"}><Button>Sign In</Button></Link>
+                <Link href={"sign-In"} ><Button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300">Sign In</Button></Link>
                 </div>
                 <div className='signup'>
-                    <Link href={"sign-Up"}><Button>Sign UP</Button></Link>
+                    <Link href={"sign-Up"} ><Button className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300">Sign UP</Button></Link>
                 </div>
             </div>
         </div>
