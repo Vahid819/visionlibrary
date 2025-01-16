9417760bde2b632f9fb26a7c5448238c209dc13b
vahid819 <vahidmomin072@gmail.com>
2025-01-16 14:47:03 +0530

    delete all fiels

--- src/zodSchemas/signIn.js
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod';
-
-const loginSchema = z.object({
-    username: z.string().min(4, "username must be atlast 4 carector"),
-    password: z.string().min(8, 'In password at last 8 carectors are required'),
-})
-
-export default loginSchema;
