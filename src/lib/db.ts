f64488ad03d843bc7e3e94e368942ecb5a3701d2
vahid819 <vahidmomin1819@gmail.com>
2026-02-05 18:52:31 +0530

    mongodb connection successfully

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,25 @@
-import { log } from "console";
 import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
     if (mongoose.connection.readyState === 1) {
         console.log("mongodb already connected");
+        return;
     } else {
         try {
             await mongoose.connect(process.env.MONGO_URL!);
-            console.log("mongodb connected");
+            const connection = mongoose.connection
+
+            mongoose.connection.on("connected", ()=>{
+                console.log("mongodb connection successfully", mongoose.connection.readyState);
+            })
+
+            mongoose.connection.on("error", (error)=>{
+                console.log("mongodb connection error please make sure database connection", mongoose.connection.readyState, error.message);
+
+                process.exit();
+                
+            })
             
         } catch (error: any) {
             console.log("mongodb connection error: ", error.message);
