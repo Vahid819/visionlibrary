92fa53e98f76b65c228f0fade3b68180e532019b
Vahid Momin <vahidmomin.dev@gmail.com>
2026-03-29 11:13:58 +0530

    database connection file ready

---
+++ src/lib/db.js
@@ -0,0 +1,43 @@
+import "server-only";
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+if (!MONGODB_URL) {
+  throw new Error("Please define the MONGODB_URL environment variable.");
+}
+
+const globalWithMongoose = globalThis;
+
+const cached = globalWithMongoose.mongoose || {
+  conn: null,
+  promise: null,
+};
+
+globalWithMongoose.mongoose = cached;
+
+export async function connectDB() {
+    //TODO after complete all code remove console.log
+  if (cached.conn) {
+    console.log(cached.conn);
+    return cached.conn;
+  } 
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
+
