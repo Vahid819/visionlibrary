e9305feda73d279116fd28ab4ee222ca48f4100c
Vahid Momin <vahidmomin.dev@gmail.com>
2026-03-31 08:51:30 +0530

    small changes like delete the otp route and add on sugnup route

--- src/app/api/auth/signup/route.js
+++ src/app/api/auth/signup/route.js
@@ -1,20 +1,33 @@
 import connectDB from "@/lib/db";
 import UserModel from "@/models/User";
 import { hash } from "bcryptjs";
+import crypto from "crypto";
 
 export async function POST(req){
     await connectDB();
 
     try {
         const body = await req.json();
+        const existingUser = await UserModel.findOne({email: body.email});
+
+        if(existingUser){
+            return new Response(JSON.stringify({ error: "Email already in use" }), { status: 400 });
+        }
+
+        const otp = crypto.randomInt(100000, 999999); // Generate a 6-digit OTP
+        const otpExpiryDate = new Date(Date.now() + 1 * 60 * 1000); // OTP expires in 1 minute
         const hashedPassword = await hash(body.password, 10);
+
         const newUser = new UserModel({
             name: body.name,
             lastname: body.lastname,
             email: body.email,
             password: hashedPassword,
-            otp: body.otp,
+            otp: otp,
+            otpCreatedAt: new Date(),
+            otpExpiryDate: otpExpiryDate,
         })
+
         await newUser.save();
         return new Response(JSON.stringify({ message: "User registered successfully" }), { status: 201 });
     } catch (error) {
