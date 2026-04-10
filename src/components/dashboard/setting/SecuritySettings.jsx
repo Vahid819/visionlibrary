91ffc7e4c9e7e7a12d1cd87099f8e5f40a433d4a
Vahid Momin <vahidmomin.dev@gmail.com>
2026-04-10 09:48:06 +0530

    zod validation add succesully

--- src/components/dashboard/setting/SecuritySettings.jsx
+++ src/components/dashboard/setting/SecuritySettings.jsx
@@ -1,8 +1,17 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { userSchema } from "@/zodSchema/userSchema";
 
 export default function SecuritySettings({ form, onChange, disabled }) {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(userSchema),
+  });
   return (
     <Card>
       <CardHeader>
@@ -11,11 +20,13 @@ export default function SecuritySettings({ form, onChange, disabled }) {
       <CardContent className="grid md:grid-cols-2 gap-4">
         <div className="flex flex-col gap-2">
           <Label>New Password</Label>
-          <Input type="password" name="password" placeholder="New Password" value={form.password} onChange={onChange} disabled={disabled} />
+          <Input type="password" name="password" placeholder="New Password" {...register("password")} value={form.password} onChange={onChange} disabled={disabled} />
+          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
         </div>
         <div className="flex flex-col gap-2">
           <Label>Confirm Password</Label>
-          <Input type="password" name="confirmPassword" placeholder="Confirm Password" disabled={disabled} />
+          <Input type="password" name="confirmPassword" placeholder="Confirm Password" {...register("confirmPassword")} value={form.confirmPassword} onChange={onChange} disabled={disabled} />
+          {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
         </div>
       </CardContent>
     </Card>
