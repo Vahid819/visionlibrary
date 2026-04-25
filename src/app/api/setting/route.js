44f163d1147630d3c447b1d544b5e6b86ea1453b
Vahid Momin <vahidmomin.dev@gmail.com>
2026-04-25 17:15:37 +0530

    fix setting api route

--- src/app/api/setting/route.js
+++ src/app/api/setting/route.js
@@ -1,3 +1,8 @@
+import Seat from "@/models/Seats";
+import connectDB from "@/lib/db";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
 export async function POST(req) {
   const session = await getServerSession(authOptions);
   await connectDB();
